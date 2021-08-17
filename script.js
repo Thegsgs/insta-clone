f291461fda3a193abe118765147026660b8eb5d4
Simon <simonshrb@gmail.com>
2021-08-17 13:41:33 +0300

    js hotfix

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 let editForm = document.querySelector('.edit-form');
 let overlay = document.querySelector('.overlay');
-let editButton = document.querySelector('.profile__button-edit')
-let closeButton = document.querySelector('.edit-form__button-close')
+let editButton = document.querySelector('.profile__button-edit');
+let closeButton = document.querySelector('.edit-form__button-close');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+let saveButton = document.querySelector('.edit-form__button-submit');
+let inputJob = document.querySelector('.edit-form__input_type_job');
+let inputName = document.querySelector('.edit-form__input_type_name');
 
 function makevisible() {
+  inputName.value = profileName.textContent;
+  inputJob.value = profileJob.textContent;
   overlay.classList.add('visible');
 }
 
@@ -13,15 +20,6 @@ function makeinvisible() {
 
 editButton.addEventListener('click', makevisible);
 closeButton.addEventListener('click', makeinvisible);
-
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__job');
-let saveButton = document.querySelector('.edit-form__button-submit');
-let inputJob = document.querySelector('.edit-form__input_type_job');
-let inputName = document.querySelector('.edit-form__input_type_name');
-
-inputName.value = profileName.textContent;
-inputJob.value = profileJob.textContent;
 
 function changeInfo(evt) {
   evt.preventDefault();
