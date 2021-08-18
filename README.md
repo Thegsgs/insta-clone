0dc008690384b9df4401ef964eac11266a74b9ea
Simon <simonshrb@gmail.com>
2021-08-18 19:10:28 +0300

    fix: popup size fixes mobile

--- README.md
+++ README.md
@@ -4,5 +4,8 @@
 Project number 4 as part of my attendance of the Practicum by Yandex coding bootcamp.
 This time we used Javascript to make a popup window appear and edit text in the profile section.
 
+## Tech used:
+I used BEM to structure the files and classes, flexbox and grid to style the elements on the page and pure JS (no libraries) to make the page interactive.
+
 ## Github Pages link:
 https://thegsgs.github.io/web_project_4/
--- scripts/script.js
+++ scripts/script.js
@@ -4,26 +4,28 @@ let editButton = document.querySelector('.profile__button-edit');
 let closeButton = document.querySelector('.popup__button-close');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
-let saveButton = document.querySelector('.popup__button-submit');
 let inputJob = document.querySelector('.popup__input_type_job');
 let inputName = document.querySelector('.popup__input_type_name');
 
 function togglePopup() {
-  inputName.value = profileName.textContent;
-  inputJob.value = profileJob.textContent;
+
   if (popup.classList.contains('popup_opened')) {
     popup.classList.remove('popup_opened');
-  } else { popup.classList.add('popup_opened'); }
+  } else {
+    inputName.value = profileName.textContent;
+    inputJob.value = profileJob.textContent;
+    popup.classList.add('popup_opened');
+  }
 }
 
 editButton.addEventListener('click', togglePopup);
 closeButton.addEventListener('click', togglePopup);
 
-function changeInfo(evt) {
+function handleFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileJob.textContent = inputJob.value;
   popup.classList.remove('popup_opened');
 }
 
-saveButton.addEventListener('click', changeInfo);
+editForm.addEventListener('submit', handleFormSubmit);
